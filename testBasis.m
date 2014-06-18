4cc6716ecb562d781062aba9bc3b0470bc28df83
Volkan Cirik <vcirik@Volkans-MacBook-Air.local>
2014-06-18 17:37:50 +0300

    corrected the outer product of arcs

--- testBasis.m
+++ testBasis.m
@@ -5,22 +5,24 @@ narcs=0;
 nwrong = 0;
 
 v0 = te_data{1}.v;
-vlength = (length(v0)+1)^2;
+vlength = (length(v0)*2)^2;
+pred_arcs = zeros(10000,vlength);
 for s = 1:S,
     arcs = te_data{s};
     T = length(arcs);
 
     narcs = narcs + T -1;
-    pred_arcs = zeros(T*T,vlength);
+    %pred_arcs = zeros(T*T,vlength);
     j = 1;
     for fr = 1:T,
         for to = 1:T,
             %    prec_arcs = predict([arcs(to).v arcs(fr).v ]) % implement your favorite ML algorithm to predict arc score
-            pred_arcs(j,:) = outerproduct([arcs(to).v 1],[arcs(fr).v 1]);
+            pred_arcs(j,:) = outerproduct([arcs(to).v arcs(fr).v],[arcs(to).v arcs(fr).v]);
             j = j+1;
         end
     end
-    score = pred_arcs*w;
+    score = pred_arcs(1:T*T,:)*w;
+%    score = pred_arcs*w;
     score = reshape(score(:),T,T);
     score(logical(eye(T))) = -99999999999;
     score(1,:) = -9999999999;
--- trainBasis.m
+++ trainBasis.m
@@ -5,8 +5,10 @@ LOG.tr_err = [];
 LOG.dev_err = [];
 
 v0 = tr_data{1}.v;
-vlength = (length(v0)+1)^2;
+vlength = (length(v0)*2)^2;
 w = zeros(vlength,1);
+pred_arcs = zeros(10000,vlength);
+
 for i = 1:it,
     LOG.narcs=0;
     LOG.nwrong = 0;
@@ -21,11 +23,11 @@ for i = 1:it,
             for to = 1:T,
 %                prec_arcs = predict([arcs(to).v arcs(fr).v ]) %implement
 %                your favorite ML algorithm to predict arc score
-                pred_arcs(j,:) = outerproduct([arcs(to).v 1],[arcs(fr).v 1]); % take outer product, it should correspond to poly kernel degree 2
+                pred_arcs(j,:) = outerproduct([arcs(to).v arcs(fr).v],[arcs(to).v arcs(fr).v]); % take outer product, it should correspond to poly kernel degree 2
                 j = j+1;
             end
         end
-        score = pred_arcs*w;
+        score = pred_arcs(1:T*T,:)*w;
         score = reshape(score(:),T,T);
         score(logical(eye(T))) = -99999999999; %%%% how come -Inf make decoder choose root as head for all tokens? That's why I use this number instead -Inf
         score(1,:) = -9999999999;
@@ -36,8 +38,8 @@ for i = 1:it,
         for tok = 2:T;
             if hguessed(tok) ~= hgold(tok)
                 
-               w = w + outerproduct([arcs(hgold(tok)).v 1],[arcs(tok).v 1])';   % Add the correct arc %% already calculated wrong one?
-               w = w - outerproduct([arcs(hguessed(tok)).v 1],[arcs(tok).v 1])';% Subtract the wrong
+               w = w + outerproduct([arcs(hgold(tok)).v arcs(tok).v ],[arcs(hgold(tok)).v arcs(tok).v ])';   % Add the correct arc %% already calculated wrong one?
+               w = w - outerproduct([arcs(hguessed(tok)).v arcs(tok).v],[arcs(hguessed(tok)).v arcs(tok).v])';% Subtract the wrong
                
                % Train your favorite ML algorithm with correct and wrong
                % arcs
